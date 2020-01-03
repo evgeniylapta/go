0894d074661daa59c845942c7a0c83cda9809830
Eugene Lapta <eugene.lapta@exposit.com>
2020-01-03 15:39:41 +0300

    move db connection in separate package

--- commandline/worldgenerate.go
+++ commandline/worldgenerate.go
@@ -1,55 +1,17 @@
 package commandline
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/evgeniylapta/go/app/village"
 	"github.com/evgeniylapta/go/app/worldmap"
-	"github.com/jinzhu/gorm"
+	"github.com/evgeniylapta/go/dbmanage"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"io/ioutil"
-	"os"
 )
 
 const GenWorldProgramParam = "generate-world"
 
-type appConfig struct {
-	Host     string `json:"host"`
-	Port     int    `json:"port"`
-	User     string `json:"user"`
-	Password string `json:"password"`
-	Dbname   string `json:"dbname"`
-}
-
-func getAppConf() appConfig {
-	jsonFile, openErr := os.Open("appconfig.json")
-
-	if openErr != nil {
-		fmt.Println(openErr)
-	}
-
-	defer jsonFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
-	var appConf appConfig
-	unmarshalErr := json.Unmarshal(byteValue, &appConf)
-	if unmarshalErr != nil {
-		fmt.Println(unmarshalErr)
-	}
-
-	return appConf
-}
-
 func WorldGenStart() {
-	appConf := getAppConf()
-
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		appConf.Host, appConf.Port, appConf.User, appConf.Password, appConf.Dbname)
-	db, err := gorm.Open("postgres", psqlInfo)
-	if err != nil {
-		panic("failed to connect database")
-	}
+	db := dbmanage.ConnectToDb()
 
 	db.AutoMigrate(&village.Village{})
 
